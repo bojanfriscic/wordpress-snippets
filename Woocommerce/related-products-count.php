faf08f9ade04febfb2dee5466808cade8a4fff47
bojanfriscic <friscic.bojan@gmail.com>
2018-04-18 16:14:19 +0200

    Related products count

---
+++ Woocommerce/related-products-count.php
@@ -0,0 +1,8 @@
+<?php 
+
+add_filter('woocommerce_output_related_products_args', 'my_related_product_count');
+  function my_related_product_count( $args ) {
+	$args['posts_per_page'] = 3; // 3 related products
+	$args['columns'] = 3; // arranged in 23 columns
+	return $args;
+}
