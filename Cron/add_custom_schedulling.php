59691db77afb75f13f5cf574089fe3920df56313
bojanfriscic <friscic.bojan@gmail.com>
2018-09-30 12:54:08 +0200

    Custom scheduling

---
+++ Cron/add_custom_schedulling.php
@@ -0,0 +1,20 @@
+<?php
+
+add_action('init', function() {
+  if(wp_next_scheduled('my_cron_hook')) {
+    wp_schedule_event(time(), 'hourly', 'my_cron_hook');
+  }
+});
+
+add_action('my_cron_hook', function() {
+  // Triggered actions go here
+});
+
+add_filter('cron_schedules', function($schedules)) {
+  $schedules['five-minutes'] = array(
+    'interval'  =>  300,
+    'display'   =>  'Every Five Minutes'
+  );
+
+  return $schedules;
+}
