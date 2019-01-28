ee3eace4f47c7c2108198493f4fff955a6e5bc56
bojanfriscic <friscic.bojan@gmail.com>
2019-01-28 12:59:37 +0100

    Register script added

---
+++ Assets/register-script.php
@@ -0,0 +1,7 @@
+<?php 
+
+add_action('init', 'my_enqueue_assets');
+function my_enqueue_assets() {
+  wp_register_script('myscript', get_theme_file_uri('/assets/js/myscript.js'));
+  ('myscript', get_template_directory_uri() . '/assets/js/myscript.js', array('jquery'), '1', true);
+}
