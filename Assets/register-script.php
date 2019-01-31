81e837844e387a8067ddf5064942e030ec00ffee
bojanfriscic <friscic.bojan@gmail.com>
2019-01-31 14:56:53 +0100

    Bug fixed

--- Assets/register-script.php
+++ Assets/register-script.php
@@ -3,5 +3,5 @@
 add_action('init', 'my_enqueue_assets');
 function my_enqueue_assets() {
   wp_register_script('myscript', get_theme_file_uri('/assets/js/myscript.js'));
-  ('myscript', get_template_directory_uri() . '/assets/js/myscript.js', array('jquery'), '1', true);
+  wp_enqueue_script('myscript', get_template_directory_uri() . '/assets/js/myscript.js', array('jquery'), '1', true);
 }
