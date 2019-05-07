c597460f6f6085be7d3a9d1283bfbca182c657d9
bojanfriscic <friscic.bojan@gmail.com>
2019-05-07 09:13:31 +0200

    Formatting fixed

--- Custom Post Types/custom-post-type.php
+++ Custom Post Types/custom-post-type.php
@@ -2,41 +2,42 @@
 
 add_action('init', 'add_my_cpt', 0);
 function add_ctp() {
-    $labels = array(
-        'name'                  => _x( 'Post Types', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Post Type', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Post Types', 'text_domain' ),
-		'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
-		'archives'              => __( 'Item Archives', 'text_domain' ),
-		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-		'all_items'             => __( 'All Items', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Item', 'text_domain' ),
-		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-		'update_item'           => __( 'Update Item', 'text_domain' ),
-		'view_item'             => __( 'View Item', 'text_domain' ),
-		'view_items'            => __( 'View Items', 'text_domain' ),
-		'search_items'          => __( 'Search Item', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' )
-    );
-    $args = array(
-        'label'                 => __( 'Post Type', 'text_domain' ),
-		'description'           => __( 'Post Type Description', 'text_domain' ),
+	$labels = array(
+		'name'                  => _x('Post Types', 'Post Type General Name', 'insert_text_domain'),
+		'singular_name'         => _x('Post Type', 'Post Type Singular Name', 'insert_text_domain'),
+		'menu_name'             => __('Post Types', 'insert_text_domain'),
+		'name_admin_bar'        => __('Post Type', 'insert_text_domain'),
+		'archives'              => __('Item Archives', 'insert_text_domain'),
+		'attributes'            => __('Item Attributes', 'insert_text_domain'),
+		'parent_item_colon'     => __('Parent Item:', 'insert_text_domain'),
+		'all_items'             => __('All Items', 'insert_text_domain'),
+		'add_new_item'          => __('Add New Item', 'insert_text_domain'),
+		'add_new'               => __('Add New', 'insert_text_domain'),
+		'new_item'              => __('New Item', 'insert_text_domain'),
+		'edit_item'             => __('Edit Item', 'insert_text_domain'),
+		'update_item'           => __('Update Item', 'insert_text_domain'),
+		'view_item'             => __('View Item', 'insert_text_domain'),
+		'view_items'            => __('View Items', 'insert_text_domain'),
+		'search_items'          => __('Search Item', 'insert_text_domain'),
+		'not_found'             => __('Not found', 'insert_text_domain'),
+		'not_found_in_trash'    => __('Not found in Trash', 'insert_text_domain'),
+		'featured_image'        => __('Featured Image', 'insert_text_domain'),
+		'set_featured_image'    => __('Set featured image', 'insert_text_domain'),
+		'remove_featured_image' => __('Remove featured image', 'insert_text_domain'),
+		'use_featured_image'    => __('Use as featured image', 'insert_text_domain'),
+		'insert_into_item'      => __('Insert into item', 'insert_text_domain'),
+		'uploaded_to_this_item' => __('Uploaded to this item', 'insert_text_domain'),
+		'items_list'            => __('Items list', 'insert_text_domain'),
+		'items_list_navigation' => __('Items list navigation', 'insert_text_domain'),
+		'filter_items_list'     => __('Filter items list', 'insert_text_domain')
+	);
+	
+	$args = array(
+		'label'                 => __('Post Type', 'insert_text_domain'),
+		'description'           => __('Post Type Description', 'insert_text_domain'),
 		'labels'                => $labels,
 		'supports'              => false,
-		'taxonomies'            => array( 'category', 'post_tag' ),
+		'taxonomies'            => array('category', 'post_tag'),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
@@ -49,8 +50,9 @@ function add_ctp() {
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
-		'supports'      		=> array('title', 'editor', 'thumbnail', 'author'),
+		'supports'      		=> array('title', 'editor', 'thumbnail'),
 		'menu_icon'				=> 'dashicons-format-status'
-    );
-    register_post_type('my_cpt', $args);
+	);
+	
+	register_post_type('my_cpt', $args);
 }
