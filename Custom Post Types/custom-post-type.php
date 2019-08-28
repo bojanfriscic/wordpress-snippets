a65b862cba3b57c8f5055d62265631a244137809
bojanfriscic <friscic.bojan@gmail.com>
2019-08-28 15:51:15 +0200

    Prop for Guttenberg support added

--- Custom Post Types/custom-post-type.php
+++ Custom Post Types/custom-post-type.php
@@ -45,6 +45,8 @@ function add_ctp() {
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
+		// Set show_in_rest to true to enable Gutenberg editor
+		'show_in_rest'					=> true,
 		'can_export'            => true,
 		'has_archive'           => true,
 		'exclude_from_search'   => false,
