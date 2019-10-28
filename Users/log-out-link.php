cfad6f923e6d9f565b43af97f9d4b2e42da6589c
bojanfriscic <friscic.bojan@gmail.com>
2019-10-28 13:36:16 +0100

    Log out link updated

--- Users/log-out-link.php
+++ Users/log-out-link.php
@@ -1,3 +1,9 @@
+<?php // With security confirmation ?>
 <a href="<?php echo esc_url(home_url('/wp-login.php?action=logout')); ?>">
   <?php _e('Log out', 'text_domain'); ?>
+</a>
+
+<?php // Without security confirmation ?>
+<a href="<?php echo wp_logout_url(); ?>">
+  <?php _e('Log out', constant('THEME\TEXT_DOMAIN')); ?>
 </a>
