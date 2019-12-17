57f595ef29c7b7fe5396ee2d4ee8826855bf4fa6
bojanfriscic <friscic.bojan@gmail.com>
2019-12-17 10:21:00 +0100

    is_admin() condition added

--- Posts/exclude-category-from-archive.php
+++ Posts/exclude-category-from-archive.php
@@ -3,7 +3,8 @@
 add_action('pre_get_posts', 'my_exclude_category_from_archive');
 function my_exclude_category_from_archive($query) {
   // Exclude from the main archive, but display on the category archive
-  if($query->is_main_query() && $query->query['category_name'] !== 'custom-category-slug') {
+  // Must add is_admin() so posts are displayed in WP Administration Posts -> All posts
+  if($query->is_main_query() && !is_admin() && $query->query['category_name'] !== 'custom-category-slug') {
     // Put category ID in the array
     $query->set('category__not_in', array(10));
   }
